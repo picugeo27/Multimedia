df3ad9a02b9e90b0f189a5b117ca89cc9f4f3fc1
picugeo27 <g.picu.2018@alumnos.urjc.es>
2023-05-11 17:17:43 +0200

    Muchas cosas

---
+++ Scripts/Pagina_principal.js
@@ -0,0 +1,9 @@
+const reaudacion_base=64550;
+const patrocinadores_base=68;
+const advertencia="Esta pagina utiliza cookies, si no está de acuerdo con ello por favor vayase a la mierda"
+
+var patrocinadores = document.getElementById("patrocinadores");
+var recaudacion = document.getElementById("recaudaciones");
+
+document.cookie = "miCookie=valorDeMiCookie";
+console.log(document.cookie.trim());
