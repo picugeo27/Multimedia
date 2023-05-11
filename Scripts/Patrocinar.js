93f3af80ba76d295e1b000129210f889b767f39b
picugeo27 <g.picu.2018@alumnos.urjc.es>
2023-05-11 22:40:30 +0200

    Update Patrocinar.js

--- Scripts/Patrocinar.js
+++ Scripts/Patrocinar.js
@@ -1,9 +1,20 @@
 var pagado = 0;
 const fecha_max = new Date(2023, 5, 5);
 
-var btn = document.getElementById('btn');
+var btn1 = document.getElementById('btn1');
 btn.addEventListener('click', (e) =>{
-	pagado = 100000 + parseInt(document.cookie.split('=')[1]);
+	pagado = 50 + parseInt(document.cookie.split('=')[1]);
+	document.cookie = "dinero=" + pagado + "; expires:" + fecha_max + ";";
+	console.log(document.cookie);
+	recaudacion.textContent = (reaudacion_base + parseInt(document.cookie.split('=')[1]));
+
+})
+
+/* Comentario uwu*/
+
+var btn2 = document.getElementById('btn2');
+btn.addEventListener('click', (e) =>{
+	pagado = 150 + parseInt(document.cookie.split('=')[1]);
 	document.cookie = "dinero=" + pagado + "; expires:" + fecha_max + ";";
 	console.log(document.cookie);
 	recaudacion.textContent = (reaudacion_base + parseInt(document.cookie.split('=')[1]));
