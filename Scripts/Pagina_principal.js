bebedebb88d21414e0eacc47c2ce67a3d9388a51
picugeo27 <g.picu.2018@alumnos.urjc.es>
2023-05-11 21:15:17 +0200

    Cerquita

--- Scripts/Pagina_principal.js
+++ Scripts/Pagina_principal.js
@@ -34,7 +34,7 @@ var pagado = 0;
 
 var btn1 = document.getElementById('btn1');
 btn1.addEventListener('click', (e) =>{
-	pagado = 100000 + parseInt(cookie.split('=')[1]);
+	pagado = 100000 + parseInt(document.cookie.split('=')[1]);
 	document.cookie = "dinero=" + pagado + ";";
 	console.log(document.cookie);
 	recaudacion.textContent = (reaudacion_base + parseInt(document.cookie.split('=')[1]));
