63ccf3ce2903dcfda3fe2913f7539bacc15d520b
picugeo27 <g.picu.2018@alumnos.urjc.es>
2023-05-11 19:21:20 +0200

    Sigo intentando las cookies

--- Scripts/Pagina_principal.js
+++ Scripts/Pagina_principal.js
@@ -8,13 +8,13 @@ var recaudacion = document.getElementById("recaudaciones");
 var dinero = document.cookie;
 console.log(document.cookie);
 
-if (dinero == 0){
+if (dinero < 0){
 	if (dinero >=0){
 
 	} else {
 	alert(advertencia);
-	document.cookie = "dinero=0";
+	document.cookie = "dinero=0"
 	}
 
 }
-console.log(dinero);
+console.log(dinero)
