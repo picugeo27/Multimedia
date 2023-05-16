0a8ed42e21a7219f00bcaecb17cbcdc47c834193
tarsotarso <129898242+tarsotarso@users.noreply.github.com>
2023-05-16 19:13:38 +0200

    Pagina_patrocinar

--- Scripts/Patrocinar.js
+++ Scripts/Patrocinar.js
@@ -1,9 +1,33 @@
 var pagado = 0;
 const fecha_max = new Date(2023, 5, 5);
 
-var btn = document.getElementById('btn');
+var btn = document.getElementById('boton_patrocinar1');
 btn.addEventListener('click', (e) =>{
-	pagado = 100000 + parseInt(document.cookie.split('=')[1]);
+	pagado = 10 + parseInt(document.cookie.split('=')[1]);
+	document.cookie = "dinero=" + pagado + "; expires:" + fecha_max + ";";
+	console.log(document.cookie);
+	recaudacion.textContent = (reaudacion_base + parseInt(document.cookie.split('=')[1]));
+
+})
+var btn = document.getElementById('boton_patrocinar2');
+btn.addEventListener('click', (e) =>{
+	pagado = 10 + parseInt(document.cookie.split('=')[1]);
+	document.cookie = "dinero=" + pagado + "; expires:" + fecha_max + ";";
+	console.log(document.cookie);
+	recaudacion.textContent = (reaudacion_base + parseInt(document.cookie.split('=')[1]));
+
+})
+var btn = document.getElementById('boton_patrocinar3');
+btn.addEventListener('click', (e) =>{
+	pagado = 10 + parseInt(document.cookie.split('=')[1]);
+	document.cookie = "dinero=" + pagado + "; expires:" + fecha_max + ";";
+	console.log(document.cookie);
+	recaudacion.textContent = (reaudacion_base + parseInt(document.cookie.split('=')[1]));
+
+})
+var btn = document.getElementById('boton_patrocinar4');
+btn.addEventListener('click', (e) =>{
+	pagado = 10 + parseInt(document.cookie.split('=')[1]);
 	document.cookie = "dinero=" + pagado + "; expires:" + fecha_max + ";";
 	console.log(document.cookie);
 	recaudacion.textContent = (reaudacion_base + parseInt(document.cookie.split('=')[1]));
