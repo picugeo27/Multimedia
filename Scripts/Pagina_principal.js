75be3de4eac140ba472dcc5afdbcd4474d3c8b1d
picugeo27 <g.picu.2018@alumnos.urjc.es>
2023-05-16 23:39:40 +0200

    Ultimo Update

--- Scripts/Pagina_principal.js
+++ Scripts/Pagina_principal.js
@@ -1,6 +1,6 @@
 const reaudacion_base=64549;
 const patrocinadores_base=68;
-const advertencia="Esta pagina utiliza cookies, si no está de acuerdo con ello por favor vayase a la mierda";
+const advertencia="Esta pagina utiliza cookies, ¿quieres galletas?";
 
 const fecha_max = new Date(2023, 5, 5);	// el mes empieza en 0, entonces enero es 0 y diciembre 11. 5 es junio
 const fecha_actual = new Date();
@@ -32,7 +32,7 @@ recaudacion.textContent = (reaudacion_base + parseInt(dinero.split('=')[1])) + "
 var quedan = fecha_max - fecha_actual;						// resultado en milisegundos
 quedan = quedan / (1000 * 60 * 60 * 24);					// quedan / 1000 es el resultado en segundos, despues /60 para minutos, /60 para horas 
 															// y /24 porque el dia tiene 24h, así tenemos el resultado en segundos
-dias_restantes.textContent = Math.floor(quedan) + " dias"; 	// redondeamos hacia abajo
+dias_restantes.textContent = Math.ceil(quedan) + " dias"; 	// redondeamos hacia abajo
 
 
 
